9b6dfbc3873eaca64929c6a5b57c3f39da4decff
Dmitry Boldyrev <dmtrbld@gmail.com>
2020-04-01 17:53:53 +0300

    Создана первоначальная реализация сохранения и загрузки пользователя.

--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Fitness.BL.Controller;
+using System;
+
 
 namespace Fitness.CMD
 {
@@ -10,6 +8,20 @@ namespace Fitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Добро пожаловать в приложение Fitness");
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения");
+            var birthdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите рост");
+            var height = double.Parse(Console.ReadLine());
+            var userController = new UserController(name, gender, birthdate, weight, height);
+            userController.Save();
+
         }
     }
 }
