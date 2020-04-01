45584f7c5352832f2108a5a54555eac10fc36237
Dmitry Boldyrev <dmtrbld@gmail.com>
2020-04-01 14:02:00 +0300

    Создана модель Пользователя

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Fitness.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+       /// <summary>
+       /// Создать новый пол
+       /// </summary>
+       /// <param name="name">имя пола</param>
+        public Gender(string name) 
+        {
+            if (string.IsNullOrWhiteSpace(name)) 
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым", nameof(name));
+            }
+
+            Name = name;
+
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
